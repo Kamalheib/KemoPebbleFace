4d2c2468942cda8418ef8eac29df8f8211390b59
Kamal Heib <kamalheib1@gmail.com>
2016-06-11 18:59:20 +0300

    Kemo: Add more text layers
    Add more text layers:
    1- Kemo text layer
    2- Date text layer

--- src/main.c
+++ src/main.c
@@ -1,16 +1,21 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static TextLayer *s_center_layer;
+static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
+static TextLayer *s_text_layer;
 
 static void update_time() {
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
+    static char time_buffer[8];
+    static char date_buffer[16];
     
-    static char s_buffer[8];
-    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+    strftime(time_buffer, sizeof(time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+    strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
     
-    text_layer_set_text(s_center_layer, s_buffer);
+    text_layer_set_text(s_time_layer, time_buffer);
+    text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -21,26 +26,48 @@ static void main_window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
     
-    s_center_layer = text_layer_create(GRect(0, 60, bounds.size.w, 45));
-    text_layer_set_font(s_center_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-    text_layer_set_text_alignment(s_center_layer, GTextAlignmentCenter);
-    layer_add_child(window_layer, text_layer_get_layer(s_center_layer));
+    s_text_layer = text_layer_create(GRect(0, 30, bounds.size.w, 30));
+    text_layer_set_text(s_text_layer, "KeMo");
+    text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+    text_layer_set_text_color(s_text_layer, GColorWhite);
+    text_layer_set_background_color(s_text_layer, GColorClear);
+    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+    layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+    
+    s_time_layer = text_layer_create(GRect(0, 65, bounds.size.w, 55));
+    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+    text_layer_set_text_color(s_time_layer, GColorBlack);
+    text_layer_set_background_color(s_time_layer, GColorWhite);
+    layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+    
+    s_date_layer = text_layer_create(GRect(0,120, bounds.size.w, 30));
+    text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+    text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+    text_layer_set_text_color(s_date_layer, GColorWhite);
+    text_layer_set_background_color(s_date_layer, GColorClear);
+    layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 }
 
 static void main_window_unload(Window *window) {
-    text_layer_destroy(s_center_layer);
+    text_layer_destroy(s_time_layer);
 }
 
 static void init() {
-
+    
     s_main_window = window_create();
+    
+    window_set_background_color(s_main_window, GColorDarkCandyAppleRed);
+    
     window_set_window_handlers(s_main_window, (WindowHandlers) {
         .load = main_window_load,
         .unload = main_window_unload
     });
-    
-    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
     window_stack_push(s_main_window, true);
+    update_time();
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
 }
 
 static void deinit(){
